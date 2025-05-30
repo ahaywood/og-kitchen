09af981972146a8570d3e220f9a074c1a430f1b9
Amy Haywood Dutton <me@amyhaywood.com>
2025-05-30 14:56:52 -0400

    ✨ Updated OG image tag for home page

--- dynamic-og-images/src/app/pages/Home.tsx
+++ dynamic-og-images/src/app/pages/Home.tsx
@@ -3,6 +3,8 @@ import { RequestInfo } from "rwsdk/worker";
 export function Home({ ctx }: RequestInfo) {
   return (
     <div>
+      <meta name="og:title" content="Hello, Amy!" />
+      <meta name="og:image" content="/og-react" />
       <p>
         {ctx.user?.username
           ? `You are logged in as user ${ctx.user.username}`
